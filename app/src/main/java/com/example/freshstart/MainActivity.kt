539ea1f6ba9926effb48bf70e42bac2eec0a2266
Marta <marta.dulibic@photomath.com>
2023-04-02 12:37:56 +0200

    clean up

--- app/src/main/java/com/example/freshstart/MainActivity.kt
+++ app/src/main/java/com/example/freshstart/MainActivity.kt
@@ -6,24 +6,46 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.freshstart.navigation.FreshStartNavHost
+import com.example.freshstart.navigation.IAppNavigator
+import com.example.freshstart.navigation.NavigationEffects
+import com.example.freshstart.navigation.composable
+import com.example.freshstart.navigation.destination.NoArgsDestination
 import com.example.freshstart.ui.theme.FreshStartTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var appNavigator: IAppNavigator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val navController = rememberNavController()
+            NavigationEffects(
+                navigationChannel = appNavigator.navigationChannel,
+                navHostController = navController
+            )
+
             FreshStartTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    FreshStartNavHost(
+                        navController = navController,
+                        startDestination = NoArgsDestination.SplashScreen
+                    ) {
+                        composable(destination = NoArgsDestination.SplashScreen) {
 
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/freshstart/navigation/destination/BottomBarDestination.kt
+++ app/src/main/java/com/example/freshstart/navigation/destination/BottomBarDestination.kt
@@ -5,5 +5,9 @@ sealed class BottomBarDestination(
     route: String,
     val icon: Int
 ) : NoArgsDestination(route) {
+    companion object {
+        val subClasses: List<BottomBarDestination> = listOf(
 
+        )
+    }
 }
